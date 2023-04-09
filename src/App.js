a58c1f40b2e8e5f531a4fcbea5658053f8c5ccdc
John Dana <john@danasd.com>
2023-04-09 16:24:25 -0600

    Add the property layer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
-import tasks from './data.js';
-import moment from 'moment';
+import "./App.css";
+import primaryData from "./data.js";
+import moment from "moment";
 
 function formatDate(date) {
-  return date ? moment(date).format('MM/DD/YYYY') : '';
+  return date ? moment(date).format("MM/DD/YYYY") : "";
 }
 
-function App() {    
-  const taskItems = tasks.map((task) => (
-    <li key={task.id}>
-      <input type="checkbox" checked={task.done} />
-      {task.firstName} {task.lastName}, Active Date: {formatDate(task.activeDate)}, Retire Date: {formatDate(task.retireDate)}
-    </li>
-  ));
+const property = primaryData.property
+
+const contactList = primaryData.contacts.map((contact) => (
+  <li key={contact.id}>
+    <input type="checkbox" checked={contact.done} />
+    {contact.firstName} {contact.lastName}, Active Date:{" "}
+    {formatDate(contact.activeDate)}, Retire Date:{" "}
+    {formatDate(contact.retireDate)}
+  </li>
+));
+
+function App() {
   return (
     <>
-      <ul>{taskItems}</ul>
+      <h1>{property.retireDate && '~'} {property.propertyName}</h1>
+      <ul>{contactList}</ul>
     </>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,8 +1,24 @@
-const tasks = [
+const property = {
+  "propertyName": "ZZZ CN Test",
+  "zoneName": "Central",
+  "activeDate": "2019-12-30",
+  "retireDate": null,
+  "streetNumber": 111,
+  "streetName": "Washington",
+  "streetDirection": "N",
+  "streetTypeName": "Ave",
+  "unitNumber": null,
+  "zip": null,
+  "permittedFacility": false,
+  "permitTypeName": null,
+  "industrialDischarge": false
+}; 
+const contacts = [
   { id: 0, firstName: 'Natho', lastName: 'Dana', activeDate: '2011-05-06', retireDate: null, done: true },
   { id: 1, firstName: 'Henner', lastName: 'Dana', activeDate: '2014-04-04', retireDate: '2023-04-08', done: false },
   { id: 2, firstName: 'Momma', lastName: 'Dana', activeDate: '2020-01-20', retireDate: '2023-04-08', done: false },
   { id: 3, firstName: 'Paj', lastName: 'Dana', activeDate: '2017-11-11', retireDate: '2023-04-08', done: false }
 ];
+const primaryData = { property, contacts}
 
-export default tasks;
+export default primaryData;

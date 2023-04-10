fab547fd865cc2ac14108f5c016096b3f1bc6d7a
John Dana <john@danasd.com>
2023-04-10 09:28:23 -0600

    Clean up the data.

--- src/data.js
+++ src/data.js
@@ -1,23 +1,27 @@
 const property = {
-  "propertyName": "ZZZ CN Test",
-  "zoneName": "Central",
-  "activeDate": "2019-12-30",
-  "retireDate": null,
-  "streetNumber": 111,
-  "streetName": "Washington",
-  "streetDirection": "N",
-  "streetTypeName": "Ave",
-  "unitNumber": null,
-  "zip": null,
-  "permittedFacility": false,
-  "permitTypeName": null,
-  "industrialDischarge": false
+  id: 1040,
+  propertyName: "ZZZ CN Test",
+  zoneName: "Central",
+  activeDate: "2019-12-30",
+  retireDate: null,
+  streetNumber: 111,
+  streetName: "Washington",
+  streetDirection: "N",
+  streetTypeName: "Ave",
+  unitNumber: null,
+  zip: null,
+  permittedFacility: false,
+  permitTypeName: null,
+  industrialDischarge: false
 }; 
 const contacts = [
-  { id: 0, firstName: 'Natho', lastName: 'Dana', activeDate: '2011-05-06', retireDate: null, done: true },
-  { id: 1, firstName: 'Henner', lastName: 'Dana', activeDate: '2014-04-04', retireDate: '2023-04-08', done: false },
-  { id: 2, firstName: 'Momma', lastName: 'Dana', activeDate: '2020-01-20', retireDate: '2023-04-08', done: false },
-  { id: 3, firstName: 'Paj', lastName: 'Dana', activeDate: '2017-11-11', retireDate: '2023-04-08', done: false }
+  { id: 813, firstName: 'Natho', lastName: 'Dana', activeDate: '2011-05-06', retireDate: null, done: true },
+  { id: 823, firstName: 'Henner', lastName: 'Dana', activeDate: '2014-04-04', retireDate: '2023-04-08', done: false },
+  { id: 777, firstName: 'Jim', lastName: 'Jones', activeDate: '2014-04-04', retireDate: '2023-04-08', done: false },
+  { id: 745, firstName: 'Momma', lastName: 'Dana', activeDate: '2020-01-20', retireDate: '2023-04-08', done: false },
+  { id: 111, firstName: 'Jim', lastName: 'Jones', activeDate: '2014-04-04', retireDate: '2023-04-08', done: false },
+  { id: 112, firstName: 'Paj', lastName: 'Dana', activeDate: '2017-11-11', retireDate: '2023-04-08', done: false },
+  { id: 67, firstName: 'Fred', lastName: 'Jones', activeDate: '2014-04-04', retireDate: '2023-04-08', done: false }
 ];
 const primaryData = { property, contacts}
 
